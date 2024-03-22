083072fd5d667652bf754db2a6da08db67bb916c
jrd-learn <jrd.learn@gmail.com>
2024-03-22 01:10:47 -0300

    Add project files.

---
+++ Controle_de_Velocidade_do_Robo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Controle_de_Velocidade_do_Robo
+{
+    class Robo
+    {
+        public int VelocidadeAtual { get; set; } = 0;
+        public int VelocidadeMaxima { get; }
+        public int VelocidadeMinima { get; }
+
+        public Robo(int vmin, int vmax)
+        {
+            VelocidadeMinima = vmin;
+            VelocidadeMaxima = vmax;
+            VelocidadeAtual = vmin;
+        }
+
+        public void Acelerar()
+        {
+            if (VelocidadeAtual < VelocidadeMaxima)
+            {
+                VelocidadeAtual++;
+            }
+        }
+
+        public void Desacelerar()
+        {
+            if (VelocidadeAtual > VelocidadeMinima)
+            {
+                VelocidadeAtual--;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] velocidades = Console.ReadLine().Split(' ');
+            int vMin = int.Parse(velocidades[0]);
+            int vMax = int.Parse(velocidades[1]);
+            string comandos = Console.ReadLine();
+
+            Robo robo = new Robo(vMin, vMax);
+
+            foreach(char c in comandos)
+            {
+                if(c == 'A')
+                {
+                    robo.Acelerar();
+                }
+                if(c == 'D')
+                {
+                    robo.Desacelerar();
+                }
+            }
+
+            Console.WriteLine(robo.VelocidadeAtual);
+        }
+    }
+}
---
+++ Primeiro_Desafio_Com_POO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Primeiro_Desafio_Com_POO
+{
+    internal class Program
+    {
+        class Pessoa
+        {
+            private string nome;
+            private int idade;
+            public Pessoa(string n, int i)
+            {
+                nome = n;
+                idade = i;
+            }
+
+            public string GetNome()
+            {
+                return nome;
+            }
+
+            public int GetIdade()
+            {
+                return idade;
+            }
+
+            public override string ToString()
+            {
+                return ($"Nome {nome}, idade: {idade}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            int idade = int.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(nome, idade);
+
+            Console.WriteLine($"Nome: {pessoa.GetNome()}, Idade: {pessoa.GetIdade()}");
+
+            // Console.WriteLine(pessoa);
+        }
+    }
+}
